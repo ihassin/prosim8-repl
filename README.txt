6bad049b083ea4a6248118189d21295ff55d0efe
Itamar Hassin <ihassin@mac.com>
2014-05-30 15:06:24 -0400

    Added readme text

--- README.txt
+++ README.txt
@@ -1,3 +1,12 @@
- gcc -o serial serial.c /usr/local/lib/libncursesw.a
- ./serial /dev/tty.usbserial-141
- 
+Introduction
+
+This is a REPL for the FLUKE ProSim8 device.
+
+Dependencies
+
+Curses library
+
+Compilation
+
+gcc -o serial serial.c /usr/local/lib/libncursesw.a
+./serial /dev/tty.usbserial-141
--- serial.c
+++ serial.c
@@ -1,50 +1,3 @@
-#if 0
-#include <sys/ioctl.h>
-#include <fcntl.h>
-#include <termios.h>
-
-/* we need a termios structure to clear the HUPCL bit */
-struct termios tio;
-
-int main(int argc, char *argv[])
-{
-  int fd;
-  int status;
-
-  if (argc != 4)
-  {
-    printf("Usage: setSerialSignal port                  DTR RTS\n");
-    printf("Usage: setSerialSignal /dev/ttyS0|/dev/ttyS1 0|1 0|1\n");
-    exit( 1 );
-  }
-
-  if ((fd = open(argv[1],O_RDWR)) < 0)
-  {
-    printf("Couldn't open %s\n",argv[1]);
-    exit(1);
-  }
-  tcgetattr(fd, &tio);          /* get the termio information */
-  tio.c_cflag &= ~HUPCL;        /* clear the HUPCL bit */
-  tcsetattr(fd, TCSANOW, &tio); /* set the termio information */
-
-  ioctl(fd, TIOCMGET, &status); /* get the serial port status */
-
-  if ( argv[2][0] == '1' )      /* set the DTR line */
-    status &= ~TIOCM_DTR;
-  else
-    status |= TIOCM_DTR;
-
-  if ( argv[3][0] == '1' )      /* set the RTS line */
-    status &= ~TIOCM_RTS;
-  else
-    status |= TIOCM_RTS;
-
-  ioctl(fd, TIOCMSET, &status); /* set the serial port status */
-
-  close(fd);                    /* close the device file */
-}
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -55,10 +8,9 @@ int main(int argc, char *argv[])
 #include <sys/ioctl.h>
 #include <curses.h>
 
+// /dev/tty.usbserial-142
 int main (int argc, const char * argv[]) 
 {
-// /dev/tty.usbserial-142
-
     char buf[255];
     struct termios theTermios;
     int res;
